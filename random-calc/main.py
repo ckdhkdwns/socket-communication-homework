c9e31a0b8a78cc95787710c4a73504990e9907b1
Wangjun Cha <66898263+ckdhkdwns@users.noreply.github.com>
2023-09-24 01:48:02 +0900

    REFACTOR: question_process 리팩토링 및 shutdown 함수 제거 좀 더 간결하게 바꿨습니다. 하지만 아직도 disconnect가 되지 않습니다. 왜일까요?

--- random-calc/main.py
+++ random-calc/main.py
@@ -21,42 +21,37 @@ def time_count(server):
     while sec < TIME_OUT:
         sec += 1
         time.sleep(1)
+    print(">> [{}] The total sum is {}".format(sec, sum(client_results)))
     server.close()
-    sys.exit(">> [{}] The result is {}".format(sec, sum(client_results)))
+
+    # for c in client_sockets:
+    #     c.send(">> Disconnected.".encode('utf-8'))
 
 
 def question_process(client, addr):
     eq = equation.create()
-    client.send(eq.encode('utf-8'))
-
-    print('>> [{}] {:4} {}: {}'.format(sec, "To", addr[0], eq)) # 서버 -> 클라이언트
-
-    result = int(client.recv(1024).decode('utf-8')) # 만약 받은 데이터가 숫자가 아닐 경우 client_thread에서 캐치되서 연결종료
-    answer = int(eval(eq))
-    client_results.append(result)
-
     while True:
+        client.send(eq.encode('utf-8'))
+        print('>> [{}] {:4} {}: {}'.format(sec, "To", addr[0], eq))  # 서버 -> 클라이언트
+        result = int(client.recv(1024).decode('utf-8'))  # 만약 받은 데이터가 숫자가 아닐 경우 client_thread에서 캐치되서 연결종료
+
+        answer = int(eval(eq))
+        client_results.append(result)
         print('>> [{}] {:4} {}: {}'.format(sec, "From", addr[0], str(result))) # 클라이언트 -> 서버
+
         if answer == int(result): # 정답일 경우
             print('>> [{}] Correct.'.format(sec))
-            print('>> [{}] The result is added. (Total: {})'.format(sec, sum(client_results)))
-
-            question_process(client, addr) # 재귀를 통해 다른 문제를 출제
-            break
+            eq = equation.create()
         else: # 오답일 경우
             print('>> [{}] Incorrect. The same question will be returned.'.format(sec))
-            print('>> [{}] The result is added. (Total: {})'.format(sec, sum(client_results)))
-
-            client.send(eq.encode('utf-8')) # 똑같은 문제를 다시 출제
-
-            result = int(client.recv(1024).decode('utf-8'))
-            client_results.append(result)
+        print('>> [{}] The result is added. (Total: {})'.format(sec, sum(client_results)))
 
 
 def client_thread(client, addr):
     try:
         while True:
             print(">> [{}] Client '{}' is connected".format(sec, str(addr[0])))
+            client_sockets.append(client)
             question_process(client, addr) # 문제 내기
     except Exception as e:
         print(">> [{}] Client '{}' is disconnected.".format(sec, addr[0]))
@@ -70,18 +65,17 @@ def shutdown_server_after_delay(server_socket, delay):
     print(">> [{}] Close Server.".format(sec))
 
     # 모든 클라이언트 소켓 종료
-    for client in client_sockets:
-        client.send(">> Disconnected.".encode('utf-8'))
-        client.close()
+    for cl in client_sockets:
+        print(cl)
+        cl.send(">> Disconnected.".encode('utf-8'))
+        cl.close()
 
     server_socket.close()
+    sys.exit()
 
 
 if __name__ == "__main__":
     server_socket = socket(AF_INET, SOCK_STREAM)
-
-    shutdown_thread = threading.Thread(target=shutdown_server_after_delay, args=(server_socket, TIME_OUT,))  # 60초 후 종료
-    shutdown_thread.start()
 
     try:
         server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -101,5 +95,4 @@ if __name__ == "__main__":
 
     except Exception as e:
         print(">> [{}] Error in main: {}".format(sec, e))
-    finally:
         server_socket.close()

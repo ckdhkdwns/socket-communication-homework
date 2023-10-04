e057076973c2e731914521f16a6a42739b5baab5
Wangjun Cha <66898263+ckdhkdwns@users.noreply.github.com>
2023-10-04 14:55:23 +0900

    FORMAT: 로거 포맷 추가

--- random-calc/util/logger.py
+++ random-calc/util/logger.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 class Logger:
     def __init__(self, type):
-        self.file_path = './{}_log/log_{}.txt'.format(type, dt.datetime.now())
+        self.file_path = './{}_log/log_{}.txt'.format(type, str(dt.datetime.now()).replace(" ", "-"))
         self.formatter = '>> {}'
     def log(self, message, p=True):
         if p:

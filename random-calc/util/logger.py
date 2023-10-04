6f89a0b2bc7482bb95bd54a61d6d0a3fec319085
Wangjun Cha <66898263+ckdhkdwns@users.noreply.github.com>
2023-10-04 20:54:36 +0900

    FIX: 로그 파일 경로 버그 수정 특수기호때문에 로그파일이 생성이 안 되는 버그 수정

--- random-calc/util/logger.py
+++ random-calc/util/logger.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 class Logger:
     def __init__(self, type):
-        date = str(dt.datetime.now().replace(microsecond=0)).replace(" ", "-")
+        date = str(dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
         self.file_path = './{}_log/log_{}.txt'.format(type, date)
         self.formatter = '>> {}'
     def log(self, message, p=True):
